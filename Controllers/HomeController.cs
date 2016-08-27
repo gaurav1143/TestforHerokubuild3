0bcd4bc9b0b826a680cec422ad8d70bca90ae20c
gaurav1143 <gaurav.sde13@gmail.com>
2016-08-27 21:58:21 +0530

    Add files via upload

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -2,9 +2,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNet.Mvc;
 
-namespace WebApplication1.Controllers
+namespace herokumxnet.Controllers
 {
     public class HomeController : Controller
     {
